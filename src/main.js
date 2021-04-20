fbd3a93103cf9b3d2d0ef56ec7ae15f7315b67b7
Joaco Valenverguen <llerenaluiseduardo3@gmail.com>
2021-04-20 07:00:59 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { showAlert } from "./messages";
+import "./style.css";
+
+document.getElementById("btn-alert").addEventListener("click", showAlert);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "main.bundle.js",
+  },
+  devServer: {
+    port: 5000,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.bundle.css",
+    }),
+  ],
+};
